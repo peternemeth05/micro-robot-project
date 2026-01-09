568a1ef91f6468ea9ce278f8e20ad900c30467f6
ilatham-blip <irissguyot@gmail.com>
2026-01-09 10:43:39 +0000

    actually fixed bugs

---
+++ app/lib/constants.dart
@@ -0,0 +1,10 @@
+
+
+
+class BleConstants {
+  // Service UUID: ...def0
+  static const String serviceUuid = "12345678-1234-5678-1234-56789abcdef0";
+  
+  // Command UUID: ...def1 (The "Mailbox")
+  static const String commandUuid = "12345678-1234-5678-1234-56789abcdef1";
+}
