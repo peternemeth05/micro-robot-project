02a091964c78bcf4983c257a23255ff72873c756
ilatham-blip <irissguyot@gmail.com>
2026-01-12 19:07:59 +0000

    Update geninfopage.dart

--- app/lib/pages/sensor_pages/geninfopage.dart
+++ app/lib/pages/sensor_pages/geninfopage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:robot_app/app-state2.dart';
+import 'package:robot_app/pages/controls_classes.dart/custom_joystick.dart';
 
 class GeneralInfoPage extends StatefulWidget {
   const GeneralInfoPage({super.key});
@@ -20,24 +21,71 @@ class _GeneralInfoPageState extends State<GeneralInfoPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: constraints.maxHeight/2,width:constraints.maxWidth/2,
+                Column(children: [
+                  SizedBox(height: constraints.maxHeight/4,width:constraints.maxWidth/3,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Current movement type:"+appState.path.toString()),
+                    Text("Current Movement Type: "+appState.path.name.toString()),
+                    Text("Current Joystick Position:  X:"+(appState.x*10).toInt().toString()+"  Y:"+(appState.y*10).toInt().toString())
                   ],
                 )
                 ),
-                SizedBox(height: constraints.maxHeight/2,width:constraints.maxWidth/2,
+                SizedBox(height: constraints.maxHeight/4,width:constraints.maxWidth/3,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("text"),
+                    Text("Distance to nearest surface:")
                   ],
                 )
                 ),
+                SizedBox(height: constraints.maxHeight/4,width:constraints.maxWidth/3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Current Speed:")
+                  ],
+                )
+                ),
+                ],),
+                Column(children: [
+                  SizedBox(height: constraints.maxHeight*3/4,width:constraints.maxWidth*2/3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomJoystick(),
+                  ],
+                )
+                ),
+                ],)
               ],
-            )
+            ),
+            Row(children: [
+              SizedBox(height: constraints.maxHeight/4,width:constraints.maxWidth/3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: null, child: Text("Begin Distance Recording"))
+                  ],
+                )
+              ),
+              SizedBox(height: constraints.maxHeight/4,width:constraints.maxWidth/3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: null, child: Text("End Distance Recording"))
+                  ],
+                )
+              ),
+              SizedBox(height: constraints.maxHeight/4,width:constraints.maxWidth/3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: null, child: Text("Get Current Distance"))
+                  ],
+                )
+              )
+            ],)
           ],
         );
       },
